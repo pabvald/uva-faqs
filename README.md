dd729e96c2bfeeff797814d4b59e42b4396c6d1f
pabvald <valdunciel98@gmail.com>
2020-03-23 13:50:35 +0100

    README

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
-# uva_faqs
+# Preguntas frecuentes de la Universidad de Valladolid (UVA)
+Obtención y procesamiento de las preguntas frecuentes (FAQs) de la [página de Relaciones Internacionales](https://relint.uva.es/internacional/espanol/estudiantes/guia-bienvenida/preguntas-frecuentes/) y la [paǵina de Escuela de Doctorado](https://escueladoctorado.uva.es/export/sites/doctorado/faqs/AAFF/?lang=es) de la Universidad de Valladolid en **Español** e **Inglés**. 
 
-Obtención y procesamiento de las preguntas frecuentes (FAQs) de la página de Relaciones Internacionales y la Escuela de Doctorado de la Universidad de Valladolid
+Las preguntas son almacenadas tanto en formato CSV como en formato JSON. Cada fichero posee los siguientes 2 campos:
+
+- **Question**: contiene la pregunta.
+- **Answer**: contiene la respuesta.
+
+Descomentando las líneas de código comentadas, existe la posibilidad de añadir a mayores:
+
+- **Section**: indica la sección a la que pertenece la pregunta.
+
+### Ejecución
+El fichero [main.py](./main.py) descarga las paǵinas HTML, extrae los pares de preguntas y respuestas y los almacena en el formato descrito en ficheros CSV y JSON. Este repositorio ya contiene los ficheros HTML, así como los pares pregunta-respuesta extraidos en ficheros CSV y JSON resultantes de la ejecución de [main.py](./main.py).
+
+Si desea ejecutar el fichero [main.py](./main.py) para volver a extraer los pares pregunta-respuesta, instale las dependencias y ejecute en la consola el comando
+```
+python main.py 
+```
+situándose en el directorio raíz del repositorio.
+
+### Dependencias 
+```
+pandas==1.0.3
+bs4==0.0.1
+```
+<hr>
+
+# University of Valladolid (UVA) FAQS 
+Obtaining and processing of the frequently asked questions (FAQs) from the [International Relations page](https://relint.uva.es/internacional/english/students/welcome-guide/faq/) and the [School Doctorate page](https://escueladoctorado.uva.es/export/sites/doctorado/faqs/AAFF/?lang=en) of the University of Valladolid in **Spanish** and **English**.
+
+The questions are stored in both CSV and JSON formats. Each file has the following 2 fields:
+
+- **Question**: contains the question.
+- **Answer**: contains the answer.
+
+Uncommenting the commented lines of code, there is the possibility of adding:
+
+- **Section**: indicates the section to which the question belongs.
+
+
+### Execution
+The [main.py](./main.py)  file fetches the HTML pages, extracts the question and answer pairs and stores them in the format specified in CSV and JSON. This repository already contains the HTML data, as well as the question and answer pairs extracted in CSV and JSON terms results of the execution of [main.py](./main.py).
+
+If you want to run the [main.py](./main.py) file to re-extract the question-answer pairs, install the requirements and run in console the command
+```
+python main.py
+```
+being located in the root directory of the repository.
+
+### Requirements
+```
+pandas == 1.0.3
+bs4 == 0.0.1
+```
---
+++ main.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ---IMPORTS ---
+import os 
+import pandas as pd 
+from re import sub
+from json import dump
+from bs4 import BeautifulSoup 
+from urllib import error
+from urllib.request import Request, urlopen
+from functools import reduce
+
+
+# --- GLOBAL VARIABLES ---
+HTML_DIR = 'raw_html'
+CSV_DIR = 'csv_files'
+JSON_DIR = 'json_files'
+INTREL_ROOT_NAME = "intrel"
+DOCTORATE_ROOT_NAME = "doctorate"
+LANGUAGES =['en', 'es'] 
+INTREL_URLS = {
+    'en':[ 'https://relint.uva.es/internacional/english/students/welcome-guide/faq/'],
+
+    'es':['https://relint.uva.es/internacional/espanol/estudiantes/guia-bienvenida/preguntas-frecuentes/']  
+} 
+DOCTORATE_URLS = {
+    'en': [ 'https://escueladoctorado.uva.es/export/sites/doctorado/faqs/AAFF/?lang=en', 
+            'https://escueladoctorado.uva.es/export/sites/doctorado/faqs/admisionYMatricula/?lang=en',
+            'https://escueladoctorado.uva.es/export/sites/doctorado/faqs/PD/?lang=en',
+            'https://escueladoctorado.uva.es/export/sites/doctorado/faqs/tesis/?lang=en',
+            'https://escueladoctorado.uva.es/export/sites/doctorado/faqs/financiacion/?lang=en',
+        ],
+
+    'es':[
+        'https://escueladoctorado.uva.es/export/sites/doctorado/faqs/AAFF/?lang=es', 
+        'https://escueladoctorado.uva.es/export/sites/doctorado/faqs/admisionYMatricula/?lang=es',
+        'https://escueladoctorado.uva.es/export/sites/doctorado/faqs/PD/?lang=es',
+        'https://escueladoctorado.uva.es/export/sites/doctorado/faqs/tesis/?lang=es',
+        'https://escueladoctorado.uva.es/export/sites/doctorado/faqs/financiacion/?lang=es',
+    ] 
+} 
+
+
+def make_dirs():
+    """ Creates the different directories if they don't already exist """
+    print("Creating directories...")
+    for lang in LANGUAGES:
+        # Create directory        
+        try:
+            os.makedirs('{}/{}'.format(HTML_DIR, lang))
+            os.makedirs('{}/{}'.format(JSON_DIR, lang))
+            os.makedirs('{}/{}'.format(CSV_DIR, lang))
+        except FileExistsError:
+            # Directory already exists
+            pass
+
+def gen_file_path(main_dir, lang, root, i):
+    """ Combines a root name and an index into an HTML file name """
+    return "{}/{}/{}{}.html".format(main_dir, lang, root, i)
+
+def download_html_intrel():
+    """ Downloads the UVA International Relationships' FAQs """
+    print("Downloading 'intrel' HTML files...")
+    for lang in LANGUAGES:       
+        for i, url in enumerate(INTREL_URLS[lang]):
+            file_path = gen_file_path(HTML_DIR, lang, INTREL_ROOT_NAME, i+1)
+
+            try:
+                fin = open(file_path, 'rb')
+                fin.close()
+            except FileNotFoundError:
+                # File doesn't exist
+                req = Request(url,  headers={'User-Agent': 'Mozilla/5.0'})
+                webContent = urlopen(req).read()
+                fout = open(file_path, 'wb')
+                fout.write(webContent)
+                fout.close() 
+
+def download_html_doctorate():
+    """ Downloads the UVA Doctorate School's FAQs """
+    print("Downloading 'intrel' HTML files...")
+    for lang in LANGUAGES:   
+        for i, url in enumerate(DOCTORATE_URLS[lang]):
+            file_path = gen_file_path(HTML_DIR, lang, DOCTORATE_ROOT_NAME, i+1)
+
+            try:
+                fin = open(file_path, 'rb')
+                fin.close()
+            except FileNotFoundError:
+                # File doesn't exist
+                req = Request(url,  headers={'User-Agent': 'Mozilla/5.0'})
+                webContent = urlopen(req).read()
+                fout = open(file_path, 'wb')
+                fout.write(webContent)
+                fout.close() 
+
+def extract_qa_pairs_intrel():
+    """ Extracts all the QA pairs of doctorateX.html file and 
+        creates a list of dicts with 'answer', 'question' and 'section' values
+    """
+    print("Extracting QA pairs from 'intrel' pages...")
+    for lang in LANGUAGES:
+        intrel_qa_pairs =[]       
+        for i in range(len(INTREL_URLS[lang])):
+            # Read HTML file   
+            file_path = gen_file_path(HTML_DIR, lang, INTREL_ROOT_NAME, i+1)
+            with open(file_path, "r") as fin:
+                page = fin.read()            
+
+            # Extract QA pairs from HTML file 
+            page_qa_pairs = []     
+            content = BeautifulSoup(page, "html.parser")   
+            sec_titles = list(map(lambda x : x.get_text(), 
+                            content.find_all('div', class_='elementor-clearfix')))
+            sec_contents = content.find_all('div', {'data-accordion-type':'accordion'})   
+
+            for sec in range(len(sec_titles)):
+                q_is = sec_contents[sec].find_all('i', class_='fa-accordion-icon')
+                q_spans = reduce(lambda a, b: a+b, list(map(lambda x : x.find_parents('span'), q_is)))
+                sec_questions = list(map(lambda x : x.get_text(), q_spans))
+
+                sec_answers = list(map(lambda x : str(x.contents[0])
+                                                    .replace('<p>', '')
+                                                    .replace('</p>', '')
+                                                    .replace('</p>', ' ')
+                                                    .replace('<ul>', '')
+                                                    .replace('</ul>', '')
+                                                    .replace('<li>', ' ')
+                                                    .replace('</li>', ',')
+                                                    .replace('<br/>', ' '), 
+                                                sec_contents[sec]\
+                                                    .find_all('div', class_=['eael-accordion-content', 'clearfix'])))
+
+                for p in range(len(sec_questions)):
+                    page_qa_pairs.append({'question': sec_questions[p], 
+                                    'answer': sec_answers[p], 
+                                    #'section': sec_titles[sec]
+                                    })
+            intrel_qa_pairs.append(page_qa_pairs)
+    
+        intrel_qa_pairs = list(reduce(lambda x,y: x+y, intrel_qa_pairs)) 
+
+        # Save as JSON 
+        save_as_json('{}/{}/{}'.format(JSON_DIR, lang, INTREL_ROOT_NAME), intrel_qa_pairs)
+        # Save as CSV 
+        save_as_csv('{}/{}/{}'.format(CSV_DIR, lang, INTREL_ROOT_NAME), intrel_qa_pairs) 
+
+
+def extract_qa_pairs_doctorate():
+    """ Extracts all the QA pairs of doctorateX.html file and 
+        creates a list of dicts with 'answer', 'question' and 'section' values
+    """
+    print("Extracting QA pairs from 'doctorate' pages...")
+    for lang in LANGUAGES:
+        doctorate_qa_pairs =[]       
+        for i in range(len(DOCTORATE_URLS[lang])):
+            # Read HTML file   
+            file_path = gen_file_path(HTML_DIR, lang, DOCTORATE_ROOT_NAME, i+1)
+            with open(file_path, "r") as fin:
+                page = fin.read()            
+
+            # Extract QA pairs from HTML file 
+            page_qa_pairs = []        
+            content = BeautifulSoup(page, "html.parser")
+            
+            sec_title = content.find_all('div', class_='headline')[1].get_text()\
+                                .replace('Frequently asked questions about ', '')\
+                                .replace('Frequently Asked Questions about ', '')
+            
+            sec_content = content.find_all('div', class_='panel-group acc-v2')[1]  
+
+            sec_questions = list(map(lambda x: sub('\n[\t]+[\s]+', '', x.get_text()),
+                                    sec_content.find_all('a', class_='accordion-toggle')))
+            #process_a_tags(sec_content)
+            sec_answers = list(map(lambda x: str(x.contents[1].contents[0])
+                                            .replace('<p style=\"text-align: justify;\">', '')
+                                            .replace('</p>', ' ')
+                                            .replace('<ul>', '')
+                                            .replace('</ul>', '')
+                                            .replace('<li>', ' ')
+                                            .replace('</li>', ',')
+                                            .replace('<br/>', ' '),
+                                        sec_content.find_all('div', class_='panel-body')))
+            
+            for p in range(len(sec_questions)):
+                page_qa_pairs.append({'question': sec_questions[p], 
+                                'answer': sec_answers[p],
+                                #'section': sec_title,
+                                })
+            doctorate_qa_pairs.append(page_qa_pairs)
+    
+        doctorate_qa_pairs = list(reduce(lambda x,y: x+y, doctorate_qa_pairs)) 
+
+        # Save as JSON 
+        save_as_json('{}/{}/{}'.format(JSON_DIR, lang, DOCTORATE_ROOT_NAME), doctorate_qa_pairs)
+        # Save as CSV 
+        save_as_csv('{}/{}/{}'.format(CSV_DIR, lang, DOCTORATE_ROOT_NAME), doctorate_qa_pairs) 
+
+def save_as_json(file_name, data):
+    """ Saves the provided data as a json file """
+    with open('{}.json'.format(file_name), 'w', encoding='utf8') as fout:
+        dump(data , fout, indent=4, sort_keys=True, ensure_ascii=False)
+
+def save_as_csv(file_name, data):
+    """ Saves the provided data as a csv file """ 
+    questions = list(map(lambda p: p['question'], data))
+    answers = list(map(lambda p: p['answer'], data))
+    #sections = list(map(lambda p: p['section'], data))
+    # d = {'Section': sections, 'Question': questions, 'Answer': answers }
+    d = {'Question': questions, 'Answer': answers }
+    df = pd.DataFrame(data=d)
+    df.to_csv('{}.csv'.format(file_name), sep=',', index=False)
+
+def main():
+    """ MAIN """
+    make_dirs() 
+    download_html_intrel()
+    download_html_doctorate()
+    extract_qa_pairs_doctorate()
+    extract_qa_pairs_intrel()
+
+if __name__ == '__main__':
+    main()
